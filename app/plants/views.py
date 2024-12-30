46a9794ff3f8b034ed9951028c5070020e6efbcb
BND95618 <b_dichter@yahoo.com>
2024-12-30 08:39:19 -0800

    moved header and footer inside of body, expanded pH choice range

--- app/plants/views.py
+++ app/plants/views.py
@@ -20,7 +20,8 @@ import os
 plant_types      = ["tbd", "Annual", "Grass", "Groundcover", "Perennial", "Shrub", "Succulent", "Tree", "Vegetable", "Vine"]
 sun_exposure_opt = ["tbd", "Full Sun", "Partial Sun", "Partial Shade", "Full Shade"]
 water_rqmts_opt  = ["tbd", "Very Low", "Low", "Medium", "High", "Very High"]
-ph_opt           = ["tbd", "5.5", "5.6", "5.7", "5.8", "5.9", 
+ph_opt           = ["tbd", 
+                    "5.0", "5.1", "5.2", "5.3", "5.4", "5.5", "5.6", "5.7", "5.8", "5.9", 
                     "6.0", "6.1", "6.2", "6.3", "6.4", "6.5", "6.6", "6.7", "6.8", "6.9",
                     "7.0", "7.1", "7.2", "7.3", "7.4", "7.5", "7.6", "7.7", "7.8", "7.9",
                     "8.0"]
